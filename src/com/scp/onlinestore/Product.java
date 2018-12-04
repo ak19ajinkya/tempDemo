cbbc4ca74cfbecf4fc2fdf7ca31a35000f13bc13
ak19ajinkya <ak19ajinkya@gmail.com>
2018-12-04 15:51:36 +0530

    hjfj

--- src/com/scp/onlinestore/Product.java
+++ src/com/scp/onlinestore/Product.java
@@ -2,7 +2,6 @@ package com.scp.onlinestore;
 
 public class Product {
 	//first change on local machine
-	//pushing file....
 	static int ProductId;
 	static String ProductName;
 	static double ProductPrice;
--- src/com/scp/onlinestore/TestOnlineStore.java
+++ src/com/scp/onlinestore/TestOnlineStore.java
@@ -1,6 +1,5 @@
 package com.scp.onlinestore;
 //first change on local
-//Second change to check sync
 public class TestOnlineStore {
 
 }
