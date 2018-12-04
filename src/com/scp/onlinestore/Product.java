84820cda65a7a5f84ac2e5f73b31dae97df72acd
ak19ajinkya <ak19ajinkya@gmail.com>
2018-12-04 16:12:22 +0530

    project pushing

---
+++ src/com/scp/onlinestore/Product.java
@@ -0,0 +1,14 @@
+package com.scp.onlinestore;
+
+public class Product {
+	//first change on local machine
+	//pulling file..
+	//new update
+	//my local change
+	static int ProductId;
+	static String ProductName;
+	static double ProductPrice;
+	static String ProductCategory;
+	static String ProductDescription;
+
+}
