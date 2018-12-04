c84ea85fe5321dbcec4a3e0a079d7022e221fafa
ak19ajinkya <ak19ajinkya@gmail.com>
2018-12-04 12:46:29 +0530

    first commit

---
+++ src/com/scp/onlinestore/Product.java
@@ -0,0 +1,11 @@
+package com.scp.onlinestore;
+
+public class Product {
+	
+	static int ProductId;
+	static String ProductName;
+	static double ProductPrice;
+	static String ProductCategory;
+	static String ProductDescription;
+
+}
