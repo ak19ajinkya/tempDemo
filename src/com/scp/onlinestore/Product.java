b57a39728cafab5f3abf88efd770f962d33f1429
ak19ajinkya <ak19ajinkya@gmail.com>
2018-12-04 16:00:22 +0530

    yfuyff

--- src/com/scp/onlinestore/Product.java
+++ src/com/scp/onlinestore/Product.java
@@ -3,6 +3,7 @@ package com.scp.onlinestore;
 public class Product {
 	//first change on local machine
 	//pulling file..
+	//new update
 	static int ProductId;
 	static String ProductName;
 	static double ProductPrice;
