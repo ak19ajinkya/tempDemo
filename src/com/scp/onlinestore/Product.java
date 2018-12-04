cd9ec7e608ebe8910a81e89a200c9e72937df2d2
ak19ajinkya <45586684+ak19ajinkya@users.noreply.github.com>
2018-12-04 15:12:49 +0530

    Update Product.java

--- src/com/scp/onlinestore/Product.java
+++ src/com/scp/onlinestore/Product.java
@@ -2,6 +2,7 @@ package com.scp.onlinestore;
 
 public class Product {
 	//first change on local machine
+	//pulling file
 	static int ProductId;
 	static String ProductName;
 	static double ProductPrice;
