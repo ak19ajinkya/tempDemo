04517410a3b86a4795c7894ab3ea630f9509a1a2
ak19ajinkya <ak19ajinkya@gmail.com>
2018-12-04 15:13:48 +0530

    pushing file product

--- src/com/scp/onlinestore/Product.java
+++ src/com/scp/onlinestore/Product.java
@@ -2,6 +2,7 @@ package com.scp.onlinestore;
 
 public class Product {
 	//first change on local machine
+	//pushing file
 	static int ProductId;
 	static String ProductName;
 	static double ProductPrice;
